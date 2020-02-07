fcd84c68f1d198c8e138affe0801b225a6bb1023
LitileXueZha <1941639715@qq.com>
2020-02-07 19:40:09 +0800

    api 地址更改

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,6 +1,6 @@
 /** API 接口前缀 */
-// export const API = 'https://api.ningtaostudy.cn/v1';
-export const API = 'https://php/v1';
+export const API = 'https://api.ningtaostudy.cn/v1';
+// export const API = 'https://php/v1';
 
 /** 忽略鉴权的 API 列表 */
 export const API_OMIT_AUTH = ['/oauth'];
