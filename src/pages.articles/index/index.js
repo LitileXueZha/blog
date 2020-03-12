a76df7afd80e53b8263d6bd2cc0db5c9cf1d7ede
宁涛 <tao.n@shulidata.com>
2020-03-12 18:49:48 +0800

    全部文章页筛选

--- src/pages.articles/index/index.js
+++ src/pages.articles/index/index.js
@@ -1,19 +1,53 @@
 import './index.less';
-import { Ripple } from '../../common';
+import { Ripple, fetch } from '../../common';
 
 window.addEventListener('load', () => {
   Ripple.init();
 
-  // 监听筛选关闭
-  document.querySelector('.filters').addEventListener('click', (e) => {
-    if (e.target.tagName === 'INPUT') return;
+  initTags();
 
-    if (e.target.getAttribute('class') === 'icon icon-close') {
-      // 这里因为关闭图标放在label里，点击会触发选中，阻止之
-      e.stopPropagation();
-      e.preventDefault();
-      e.target.parentNode.previousElementSibling.checked = false;
-      // setTimeout(() => e.target.parentNode.previousElementSibling.checked = false, 0);
-    }
+  const $filters = document.querySelector('.filters');
+  const $inputs = $filters.querySelectorAll('input');
+
+  // 监听筛选关闭
+  const params = {};
+  Array.from($inputs).forEach(($input) => {
+    $input.addEventListener('change', (e) => {
+      const { name, value, type } = e.target;
+
+      switch (type) {
+        case 'radio':
+          params[name] = value;
+          break;
+        case 'checkbox': {
+          if (!params[name]) params[name] = [];
+
+          const { checked } = e.target;
+          const param = params[name];
+          const index = param.indexOf(value);
+
+          if (checked && index < 0) {
+            param.push(value);
+          } else if (!checked && index > -1) {
+            param.splice(index, 1);
+          }
+          break;
+        }
+        default:
+          break;
+      }
+
+      onChange(params);
+    });
   });
+
+  function onChange(values) {
+    console.log(values);
+  }
 });
+
+async function initTags() {
+  const data = await fetch('/tags');
+
+  console.log(data);
+}
