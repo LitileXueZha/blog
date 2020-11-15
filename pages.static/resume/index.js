6ab431d1075a9c5abceb4a74b2b27031833bd3a7
诸葛林 <ktnig@qq.com>
2020-11-16 00:58:22 +0800

    简历 hooks 简写更新，依赖包升级

--- pages.static/resume/index.js
+++ pages.static/resume/index.js
@@ -5,137 +5,128 @@ import data from './data.js';
 import './index.less';
 
 // 头部组件
-class HeaderComponent extends React.Component {
-  render() {
-    const { name, role, location } = this.props;
-    return (
-      <header className="resume-header">
-        <img src="https://tao-1252397519.cos.ap-shanghai.myqcloud.com/%E5%A4%B4%E5%83%8F2.0%E2%80%94%E2%80%94%E5%B0%8F.png" alt="头像" />
-        <div className="resume-header-content">
-          <h2 className="title">{name}</h2>
-          <p className="role">{role}</p>
-          <span className="location">{location}</span>
-        </div>
-      </header>
-    );
-  }
+function Header(props) {
+  const { name, role, location } = props;
+  return (
+    <header className="resume-header">
+      <img src="https://tao-1252397519.cos.ap-shanghai.myqcloud.com/%E5%A4%B4%E5%83%8F2.0%E2%80%94%E2%80%94%E5%B0%8F.png" alt="头像" />
+      <div className="resume-header-content">
+        <h2 className="title">{name}</h2>
+        <p className="role">{role}</p>
+        <span className="location">{location}</span>
+      </div>
+    </header>
+  );
 }
 
 //  个人简介
-class ProfileComponent extends React.Component {
-  render() {
-    return (
-      <figure className="item">
-        <figcaption className="item-title">{this.props.title}</figcaption>
-        <div className="item-content">{this.props.profile}</div>
-      </figure>
-    );
-  }
+function Profile(props) {
+  return (
+    <figure className="item">
+      <figcaption className="item-title">{props.title}</figcaption>
+      <div className="item-content">{props.profile}</div>
+    </figure>
+  );
 }
 
 //  工作经历
-class ExperienceComponent extends React.Component {
-  render() {
-    return (
-      <figure className="item">
-        <figcaption className="item-title">{this.props.title}</figcaption>
-        <div className="item-content">
-          { this.props.experience.map(x => (
-            <div key={x.name} className="item-exp">
-              <strong className="item-exp-name">{x.name}</strong>
-              <time className="item-exp-time">{x.time}</time>
-              <p className="item-exp-content">{x.content}</p>
-            </div>
-          )) }
-        </div>
-      </figure>
-    );
-  }
-}
-
-//  教育经历
-class EducationComponent extends React.Component {
-  render() {
-    return (
-      <figure className="item">
-        <figcaption className="item-title">{this.props.title}</figcaption>
-        <div className="item-content">
-        { this.props.education.map(x => (
+function Experience(props) {
+  return (
+    <figure className="item">
+      <figcaption className="item-title">{props.title}</figcaption>
+      <div className="item-content">
+        {props.experience.map(x => (
           <div key={x.name} className="item-exp">
             <strong className="item-exp-name">{x.name}</strong>
             <time className="item-exp-time">{x.time}</time>
-            { x.content.map(y => <p className="item-exp-content" key={y}>{y}</p>) }
+            <p className="item-exp-content">{x.content}</p>
           </div>
-        )) }
+        ))}
+      </div>
+    </figure>
+  );
+}
+
+//  教育经历
+function Education(props) {
+  return (
+    <figure className="item">
+      <figcaption className="item-title">{props.title}</figcaption>
+      <div className="item-content">
+      {props.education.map(x => (
+        <div key={x.name} className="item-exp">
+          <strong className="item-exp-name">{x.name}</strong>
+          <time className="item-exp-time">{x.time}</time>
+          { x.content.map(y => <p className="item-exp-content" key={y}>{y}</p>) }
         </div>
-      </figure>
-    );
-  }
+      ))}
+      </div>
+    </figure>
+  );
 }
 
 //  详细信息
-class DetailComponent extends React.Component {
-  render() {
-    const { title, details, detailsKey, commonDetails } = this.props;
-    const arr = [...commonDetails];
-    arr.splice(1, 0, details);
-    return (
-      <section className="item-right">
-        <strong className="item-right-title">{this.props.title}</strong>
-        { arr.map((x, i) => <p className="item-right-p" key={x}><span className="item-right-p-label">{detailsKey[i]}</span>{x}</p>) }
-      </section>
-    );
-  }
+function Detail(props) {
+  const { title, details, detailsKey, commonDetails } = props;
+  const arr = [...commonDetails];
+  arr.splice(1, 0, details);
+  return (
+    <section className="item-right">
+      <strong className="item-right-title">{title}</strong>
+      {arr.map((x, i) => (
+        <p className="item-right-p" key={x}>
+          <span className="item-right-p-label">{detailsKey[i]}</span>
+          {x}
+        </p>
+      ))}
+    </section>
+  );
 }
 
 //  进度条
-class Progress extends React.Component {
-  render() {
-    return (
-      <div className="progress-container">
-        <span className="progress-text">{this.props.text}</span>
-        <div className="progress-total"><div className="progress-value" style={{ width: `${this.props.value}%` }} /></div>
+function Progress(props) {
+  return (
+    <div className="progress-container">
+      <span className="progress-text">{props.text}</span>
+      <div className="progress-total">
+        <div className="progress-value" style={{ width: `${props.value}%` }} />
       </div>
-    );
-  }
+    </div>
+  );
+
 }
 
 //  专业技能
-class ExpertiseComponent extends React.Component {
-  render() {
-    return (
-      <section className="item-right">
-        <strong className="item-right-title">{this.props.title}</strong>
-        { this.props.expertise.map(x => <Progress key={x[0]} text={x[0]} value={x[1]} />) }
-      </section>
-    );
-  }
+function Expertise(props) {
+  return (
+    <section className="item-right">
+      <strong className="item-right-title">{props.title}</strong>
+      {props.expertise.map(x => <Progress key={x[0]} text={x[0]} value={x[1]} />)}
+    </section>
+  );
+
 }
 
 //  爱好
-class HobbyComponent extends React.Component {
-  render() {
-    return (
-      <section className="item-right">
-        <strong className="item-right-title">{this.props.title}</strong>
-        { this.props.hobbies.map(x => <Progress key={x[0]} text={x[0]} value={x[1]} />) }
-      </section>
-    );
-  }
+function Hobby(props) {
+  return (
+    <section className="item-right">
+      <strong className="item-right-title">{props.title}</strong>
+      { props.hobbies.map(x => <Progress key={x[0]} text={x[0]} value={x[1]} />) }
+    </section>
+  );
 }
 
 //  脚处信息
-class FooterComponent extends React.Component {
-  render() {
-    return (
-      <footer className="resume-footer">
-        <a href="#">ningtaostudy.cn/resume</a>
-        <a href="mailto:18255447846@163.com" className="mail" rel="nofollow noopener noreferrer" />
-        <a href="https://github.com/LitileXueZha" className="github" target="_blank" rel="nofollow noopener noreferrer" />
-        <a href="http://wpa.qq.com/msgrd?v=3&uin=1941639715&site=qq&menu=yes" className="qq" target="_blank" rel="nofollow noopener noreferrer" />
-      </footer>
-    );
-  }
+function Footer(props) {
+  return (
+    <footer className="resume-footer">
+      <a href="#">ningtaostudy.cn/resume</a>
+      <a href="mailto:18255447846@163.com" className="mail" rel="nofollow noopener noreferrer" />
+      <a href="https://github.com/LitileXueZha" className="github" target="_blank" rel="nofollow noopener noreferrer" />
+      <a href="http://wpa.qq.com/msgrd?v=3&uin=1941639715&site=qq&menu=yes" className="qq" target="_blank" rel="nofollow noopener noreferrer" />
+    </footer>
+  );
 }
 
 //  外部容器，切换中英文
@@ -157,25 +148,27 @@ class ProviderComponent extends React.Component {
   render() {
     const { lang } = this.state;
     const { header, profile, title, experience, education, details, detailsKey, hobbies } = data[lang];
-    return [
-      <div className="resume-container" key="1">
-        <div className="resume-col-left">
-          <HeaderComponent {...header} />
-          <div className="resume-body">
-            <ProfileComponent profile={profile} title={title[0]} />
-            <ExperienceComponent experience={experience} title={title[1]} />
-            <EducationComponent education={education} title={title[2]} />
+    return (
+      <>
+        <div className="resume-container">
+          <div className="resume-col-left">
+            <Header {...header} />
+            <div className="resume-body">
+              <Profile profile={profile} title={title[0]} />
+              <Experience experience={experience} title={title[1]} />
+              <Education education={education} title={title[2]} />
+            </div>
+          </div>
+          <div className="resume-col-right">
+            <Detail title={title[3]} details={details} detailsKey={detailsKey} commonDetails={data.details} />
+            <Expertise title={title[4]} expertise={data.expertise}/>
+            <Hobby title={title[5]} hobbies={hobbies}/>
           </div>
         </div>
-        <div className="resume-col-right">
-          <DetailComponent title={title[3]} details={details} detailsKey={detailsKey} commonDetails={data.details} />
-          <ExpertiseComponent title={title[4]} expertise={data.expertise}/>
-          <HobbyComponent title={title[5]} hobbies={hobbies}/>
-        </div>
-      </div>,
-      <FooterComponent key="2" />,
-      <button className="btn-lang" onClick={() => this.changeLang()} key="3">{lang === 'zh' ? 'English' : '中文'}</button>,
-    ];
+        <Footer />
+        <button className="btn-lang" onClick={() => this.changeLang()} >{lang === 'zh' ? 'English' : '中文'}</button>
+      </>
+    );
   }
 }
 
